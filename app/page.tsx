79d7bbe93ef2e653f97a41514b9ee02c82bf279f
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-08-31 12:42:06 +0330

    add: landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import LandingPage from '@/components/template/LandingPage/LandingPage'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    <h2>
-      hello world
-    </h2>
+      <LandingPage />
     </main>
   )
 }
---
+++ components/template/LandingPage/LandingPage.tsx
@@ -0,0 +1,29 @@
+
+const LandingPage = () => {
+  // const [backgroundImageIndex, setBackgroundImageIndex] = useState<number>(0);
+
+  // const arrayImage = [1, 10];
+
+  // function updateBackgroundImage() {
+  //   backgroundImageIndex > 0
+  //     ? setBackgroundImageIndex(0)
+  //     : setBackgroundImageIndex(backgroundImageIndex + 1);
+  // }
+
+  // useEffect(() => {
+  //   const interval = setInterval(updateBackgroundImage, 4000);
+  //   return () => clearInterval(interval);
+  // });
+
+  return (
+    <>
+      {/* <Banner image={`images/${arrayImage[backgroundImageIndex]}.jpeg`} /> */}
+      {/* <WhyUs />
+      <AboutUs />
+      <HowToOrder />
+      <Guide /> */}
+    </>
+  );
+};
+
+export default LandingPage;
