aaeb146a6893cfcda6478ed21270ce0ae064868c
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-09-01 21:24:01 +0330

    some cheange

--- components/module/Banner/Banner.tsx
+++ components/module/Banner/Banner.tsx
@@ -1,6 +1,6 @@
-"use client"
-import {useState, useEffect } from "react";
-import { Typography, Button, Divider } from "@mui/material";
+"use client";
+import { useState, useEffect } from "react";
+import { Typography, Button, Box } from "@mui/material";
 import { useRouter as useNextRouter } from "next/navigation";
 
 type Router = ReturnType<typeof useNextRouter>;
@@ -9,6 +9,7 @@ const Banner = () => {
   const router: Router = useNextRouter();
 
   const [backgroundImageIndex, setBackgroundImageIndex] = useState<number>(0);
+  const [scroll, setScroll] = useState<number>(0);
 
   const arrayImage = [1, 2];
 
@@ -22,8 +23,25 @@ const Banner = () => {
     const interval = setInterval(updateBackgroundImage, 4000);
     return () => clearInterval(interval);
   });
+  useEffect(() => {
+    const handleScroll = () => {
+      setScroll(window.scrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
+  const scroller = (value:number) => {
+    window.scrollTo({
+        top: value,
+        behavior: "smooth",
+    })
+}
 
+  console.log(scroll);
+  
   const mystyle = {
     backgroundImage: `url(images/image${arrayImage[backgroundImageIndex]}.png)`,
     margin: "0px",
@@ -39,30 +57,94 @@ const Banner = () => {
     alignItems: "center" as const,
     color: "#fff",
   };
+  
+
   return (
     <>
       <div style={mystyle}>
-        <Typography fontWeight="bold" variant="h4" textAlign="center" mt={1} mx={1}>
+        <Typography
+          fontWeight="bold"
+          variant="h4"
+          textAlign="center"
+          mt={1}
+          mx={1}
+        >
           خانه بتنی
         </Typography>
-        <Typography fontWeight="bold" variant="h4" textAlign="center" mt={1} mx={1}>
-          ما مواد ساختمانی با کیفیت بالا ارائه می‌دهیم
-        </Typography>
-        <Typography fontWeight="bold" variant="h6" textAlign="center" mt={1} mx={1}>
-        با افتخار، پیشرو در ارائه محصولات بتن آرمه با کیفیت برای ساخت آینده بهتر و پایدار
-        </Typography>
-        <Button
-          variant="contained"
-          color="warning"
-          sx={{ margin: "10px 0px" }}
-          onClick={() => {
-            router.push("/projects");
-          }}
+        <Typography
+          fontWeight="bold"
+          variant="h4"
+          textAlign="center"
+          mt={1}
+          mx={1}
         >
-          <Typography fontWeight="bold" variant="h6" textAlign="right" color="white">
-            پروژه های ما
-          </Typography>
-        </Button>
+          بهینه، مهندسی بساز
+        </Typography>
+        <Typography
+          fontWeight="bold"
+          variant="h6"
+          textAlign="center"
+          mt={1}
+          mx={1}
+        >
+          با افتخار، پیشرو در ارائه محصولات بتن آرمه با کیفیت برای ساخت آینده
+          بهتر و پایدار
+        </Typography>
+        <Box
+          sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
+          <Button
+            variant="contained"
+            color="warning"
+            sx={{ margin: "10px 10px" }}
+            onClick={()=>{
+              scroller(1600)
+            }}
+          >
+            <Typography
+              fontWeight="bold"
+              variant="h6"
+              textAlign="right"
+              color="white"
+            >
+              پروژه های ما
+            </Typography>
+          </Button>
+          <Button
+            variant="contained"
+            color="warning"
+            sx={{ margin: "10px 10px" }}
+            onClick={()=>{
+              scroller(650)
+            }}
+          >
+            <Typography
+              fontWeight="bold"
+              variant="h6"
+              textAlign="right"
+              color="white"
+            >
+              بیانیه ماموریت
+            </Typography>
+          </Button>
+          <Button
+            variant="contained"
+            color="warning"
+            sx={{ margin: "10px 10px" }}
+            onClick={()=>{
+              scroller(2716)
+            }}
+          >
+            <Typography
+              fontWeight="bold"
+              variant="h6"
+              textAlign="right"
+              color="white"
+            >
+              ارتباط با ما
+            </Typography>
+          </Button>
+        </Box>
       </div>
     </>
   );
