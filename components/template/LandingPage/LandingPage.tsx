a95a3f83ec73adac5fd2cd8d5cd0a5da8b6f9488
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-09-01 11:14:37 +0330

    add: project

--- components/template/LandingPage/LandingPage.tsx
+++ components/template/LandingPage/LandingPage.tsx
@@ -1,32 +1,19 @@
-"use client";
-import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import Banner from "@/components/module/Banner/Banner";
 import AboutUs from "@/components/module/AboutUs/AboutUs";
+import Projects from "@/components/module/Projects/Projects";
 
 const LandingPage = () => {
-  const [backgroundImageIndex, setBackgroundImageIndex] = useState<number>(0);
 
-  const arrayImage = [1, 2];
-
-  function updateBackgroundImage() {
-    backgroundImageIndex > 0
-      ? setBackgroundImageIndex(0)
-      : setBackgroundImageIndex(backgroundImageIndex + 1);
-  }
-
-  useEffect(() => {
-    const interval = setInterval(updateBackgroundImage, 4000);
-    return () => clearInterval(interval);
-  });
 
   return (
     <>
       <Box>
-        <Banner image={`images/image${arrayImage[backgroundImageIndex]}.png`} />
+        <Banner />
       </Box>
       <Box m="20px 0px">
         <AboutUs />
+        <Projects />
       </Box>
     </>
   );
