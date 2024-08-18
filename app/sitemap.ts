9b06356afd10c1d89c4b6852c694fa24d37e0a1b
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-08-18 21:55:23 +0330

    change in api fetchs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,15 @@
 export default async function sitemap() {
     const staticRoutes = ["", "/project", "/contactus"];
-    const projectRoutes = await fetch("https://khanebetoni-api.iran.liara.run/projects");
+    const projectRoutes = await fetch("https://khanebetoni-api-omega.vercel.app/projects");
     const data = await projectRoutes.json();
 
     const routes = staticRoutes.map((route:any) => ({
-        url: `https://www.khanebetoni.com${route}`,
+        url: `https://www.khanebetoni.vercel.app${route}`,
         lastModified: new Date().toString(),
     }));
 
     const projects = data.map((project:any) => ({
-        url: `https://www.khanebetoni.com/project/${project.id}`,
+        url: `https:///www.khanebetoni.vercel.app/project/${project.id}`,
         lastModified: new Date().toString(),
     }));
 
